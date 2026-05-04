b09a63dd66dee4d1800f478a06aa35ce2e59cac8
DoomSociety <gergokucu12@gmail.com>
2026-05-04 16:44:23 +0200

    Update bitfaragas.cpp

--- bitfaragas.cpp
+++ bitfaragas.cpp
@@ -1,2 +1,2 @@
 #include<bits/stdc++.h>
-int a[99],n,i;main(){for(;std::cin>>a[n];n++);std::sort(a,a+n);for(;i<n;std::cout<<a[i++]<<" ");}
+int a[99],n,i;main(){for(;std::cin>>a[n];n++);std::sort(a,a+n);std::cout<<"Kesz: ";for(;i<n;std::cout<<a[i++]<<" ");}
